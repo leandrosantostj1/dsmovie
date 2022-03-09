d39a5c055a51311c7a16e4948e0d84a23dea883a
Leandro dos Santos <leandrosantostj1@outlook.com>
2022-03-09 15:19:48 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/leandrotj1/dsmovie/entities/Score.java
@@ -0,0 +1,45 @@
+package com.leandrotj1.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	//Constructors
+	public Score() {
+		
+	}
+
+	//Getters and Setters
+	public void SetMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void SetUser(User user) {
+		id.setUser(user);
+	}
+	
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+}
